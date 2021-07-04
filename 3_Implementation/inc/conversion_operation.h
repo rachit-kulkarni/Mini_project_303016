5a294d40930512bae157b69d1171eee54db50953
SanchitKulkarni <sanchitkulkarni3@gmail.com>
2021-07-04 11:10:16 +0530

    main

---
+++ 3_Implementation/inc/conversion_operation.h
@@ -0,0 +1,23 @@
+#ifndef __CONVERSION_OPERATION_H__
+#define __CONVERSION_OPERATION_H__
+#include <stdio.h>
+#include <math.h>
+#include<string.h>
+/**
+
+Takes an 
+**/
+long int Bin_to_Dec(long int); 
+long int Bin_to_Oct(long int); 
+long int Bin_to_Hex(long int); 
+long int Dec_to_Bin(long int); 
+long int Dec_to_Oct(long int); 
+long int Dec_to_Hex(long int); 
+long int Oct_to_Bin(long int); 
+long int Oct_to_Dec(long int); 
+long int Oct_to_Hex(long int); 
+void Hex_to_Bin(char []); 
+void Hex_to_Dec(char []); 
+void Hex_to_Oct(char []); 
+ 
+#endif 
