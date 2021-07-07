c318caba83c4e9eb9ad7d56ef57259a0dcda7b59
SanchitKulkarni <sanchitkulkarni3@gmail.com>
2021-07-07 09:22:54 +0530

    main

--- 3_Implementation/inc/conversion_operation.h
+++ 3_Implementation/inc/conversion_operation.h
@@ -3,21 +3,18 @@
 #include <stdio.h>
 #include <math.h>
 #include<string.h>
-/**
 
-Takes an 
-**/
-long int Bin_to_Dec(long int); 
-long int Bin_to_Oct(long int); 
-long int Bin_to_Hex(long int); 
-long int Dec_to_Bin(long int); 
-long int Dec_to_Oct(long int); 
-long int Dec_to_Hex(long int); 
-long int Oct_to_Bin(long int); 
-long int Oct_to_Dec(long int); 
-long int Oct_to_Hex(long int); 
-void Hex_to_Bin(char []); 
-void Hex_to_Dec(char []); 
-void Hex_to_Oct(char []); 
+long int Bin_to_Dec(long int); /* Takes in the input of a binary number and converts it to Decimal*/ 
+long int Bin_to_Oct(long int); /* Takes in the input of a binary number and converts it to Octal*/
+long int Bin_to_Hex(long int); /* Takes in the input of a binary number and converts it to Hexadecimal*/
+long int Dec_to_Bin(long int); /* Takes in the input of a decimal number and converts it to binary*/
+long int Dec_to_Oct(long int); /* Takes in the input of a decimal number and converts it to octal*/
+long int Dec_to_Hex(long int); /* Takes in the input of a decimal number and converts it to hexadecimal*/
+long int Oct_to_Bin(long int); /* Takes in the input of a octal number and converts it to binary*/
+long int Oct_to_Dec(long int); /* Takes in the input of a octal number and converts it to decimal*/
+long int Oct_to_Hex(long int); /* Takes in the input of a octal number and converts it to Hexadecimal*/
+void Hex_to_Bin(char []);      /* Takes in the input of a hexadecimal number and converts it to binary*/
+void Hex_to_Dec(char []);      /* Takes in the input of a hexadecimal number and converts it to Decimal*/
+void Hex_to_Oct(char []);      /* Takes in the input of a hexadecimal number and converts it to octal*/
  
 #endif 
