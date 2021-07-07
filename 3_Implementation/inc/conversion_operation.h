eac5339cdab7eee4da654c2a8a0c223982cd7119
SanchitKulkarni <sanchitkulkarni3@gmail.com>
2021-07-07 10:09:40 +0530

    main

--- 3_Implementation/inc/conversion_operation.h
+++ 3_Implementation/inc/conversion_operation.h
@@ -15,8 +15,8 @@ long int Dec_to_Hex(long int); /* Takes in the input of a decimal number and con
 long int Oct_to_Bin(long int); /* Takes in the input of a octal number and converts it to binary*/
 long int Oct_to_Dec(long int); /* Takes in the input of a octal number and converts it to decimal*/
 long int Oct_to_Hex(long int); /* Takes in the input of a octal number and converts it to Hexadecimal*/
-void Hex_to_Bin(char []);      /* Takes in the input of a hexadecimal number and converts it to binary*/
-void Hex_to_Dec(char []);      /* Takes in the input of a hexadecimal number and converts it to Decimal*/
-void Hex_to_Oct(char []);      /* Takes in the input of a hexadecimal number and converts it to octal*/
+
+long int Hex_to_Dec(char []);      /* Takes in the input of a hexadecimal number and converts it to Decimal*/
+long int Hex_to_Oct(char []);      /* Takes in the input of a hexadecimal number and converts it to octal*/
  
 #endif 
