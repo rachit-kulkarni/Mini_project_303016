0b19186ea445f50f6e2b4cbeab2ceceba112ff0e
SanchitKulkarni <sanchitkulkarni3@gmail.com>
2021-07-10 16:28:14 +0530

    Update

--- 3_Implementation/operations_test.c
+++ 3_Implementation/operations_test.c
@@ -15,26 +15,26 @@
 void setUp(){}
 /* Required by the unity test framework */
 void tearDown(){}
-
-void test_bin_to_dec(void)
+/*Pass Test Cases */
+void test_Bin_to_Dec(void)
 {
   TEST_ASSERT_EQUAL(4, Bin_to_Dec(100));//Pass Cases
   TEST_ASSERT_EQUAL(10, Bin_to_Dec(1010));//Pass Cases
 }
 
-void test_bin_to_oct(void){
+void test_Bin_to_Oct(void){
 
   TEST_ASSERT_EQUAL(51, Bin_to_Oct(101001));//Pass Cases
   TEST_ASSERT_EQUAL(12, Bin_to_Oct(1010));//Pass Cases
 }
 
-void test_bin_to_hex(void){
+void test_Bin_to_Hex(void){
 
   TEST_ASSERT_EQUAL_HEX(0x15, Bin_to_Hex(10101));//Pass Cases
   TEST_ASSERT_EQUAL_HEX(0x79, Bin_to_Hex(1111001));//Pass Cases
 }
 
-void test_dec_to_bin(void){
+void test_Dec_to_Bin(void){
 
 TEST_ASSERT_EQUAL(101, Dec_to_Bin(5));//Pass Cases
 TEST_ASSERT_EQUAL(10011, Dec_to_Bin(19));//Pass Cases
@@ -45,6 +45,13 @@ void test_Dec_to_Oct(void){
   TEST_ASSERT_EQUAL(5, Dec_to_Oct(5));//Pass Cases
   TEST_ASSERT_EQUAL(23, Dec_to_Oct(19));//Pass Cases
 
+}
+
+void test_Dec_to_Hex(void){
+
+  TEST_ASSERT_EQUAL_HEX(0x5, Dec_to_Hex(5));//Pass Cases
+  TEST_ASSERT_EQUAL_HEX(0x13, Dec_to_Hex(19));//Pass Cases
+  TEST_ASSERT_EQUAL_HEX(0x2cd,Dec_to_Hex(717));
 }
 
 void test_Oct_to_Dec(void){
@@ -59,10 +66,16 @@ void test_Oct_to_Bin(void){
 
   TEST_ASSERT_EQUAL(1000, Oct_to_Bin(10));//Pass Cases
   TEST_ASSERT_EQUAL(11011, Oct_to_Bin(33));//Pass Cases
-
-
 }
 
+void test_Oct_to_Hex(void){
+
+  TEST_ASSERT_EQUAL_HEX(0x10, Oct_to_Hex(20));//Pass Cases
+  TEST_ASSERT_EQUAL_HEX(0x1b, Oct_to_Hex(33));//Pass Cases
+  }
+
+
+/*Fail Test Cases */
 void test_fail_bin_to_dec(void)
 {
   TEST_ASSERT_EQUAL(-1, Bin_to_Dec(1290));//Fails
@@ -70,61 +83,80 @@ void test_fail_bin_to_dec(void)
   //TEST_ASSERT_EQUAL(-1,Bin_to_Dec("ABC12"));//Fails
 }
 
+void test_fail_bin_to_oct(void){
+
+  TEST_ASSERT_EQUAL(-1, Bin_to_Oct(1234));//Fails
+  TEST_ASSERT_EQUAL(-1, Bin_to_Oct(10102));
+
+}
+
+void test_fail_bin_to_hex(void){
+
+  TEST_ASSERT_EQUAL_HEX(0xffffffff, Bin_to_Hex(123));//Pass Cases
+  TEST_ASSERT_EQUAL_HEX(0xffffffff, Bin_to_Hex(32));//Pass Cases
+}
+
 void test_fail_dec_to_bin(void){
 
-TEST_ASSERT_EQUAL(101, Dec_to_Bin(5));//Pass Cases
-TEST_ASSERT_EQUAL(10011, Dec_to_Bin(19));//Pass Cases
+TEST_ASSERT_EQUAL(-1, Dec_to_Bin(12.34));//Pass Cases
+//TEST_ASSERT_EQUAL(10011, Dec_to_Bin(19));//Pass Cases
 }
 
 void test_fail_Dec_to_Oct(void){
 
   TEST_ASSERT_EQUAL(5, Dec_to_Oct(5));//Pass Cases
   TEST_ASSERT_EQUAL(23, Dec_to_Oct(19));//Pass Cases
+}
 
+void test_fail_Dec_to_Hex(void){
+  TEST_ASSERT_EQUAL(5, Dec_to_Hex(5));//Pass Cases
+  TEST_ASSERT_EQUAL(23, Dec_to_Hex(19));
 }
 
 void test_fail_Oct_to_Dec(void){
 
-  TEST_ASSERT_EQUAL(5, Oct_to_Dec(5));//Pass Cases
-  TEST_ASSERT_EQUAL(23, Oct_to_Dec(27));//Pass Cases
-
+  TEST_ASSERT_EQUAL(-1, Oct_to_Dec(8));//Pass Cases
+  TEST_ASSERT_EQUAL(-1, Oct_to_Dec(9));//Pass Case-1
 
 }
 
 void test_fail_Oct_to_Bin(void){
 
-  TEST_ASSERT_EQUAL(1000, Oct_to_Bin(10));//Pass Cases
-  TEST_ASSERT_EQUAL(11011, Oct_to_Bin(33));//Pass Cases
+  TEST_ASSERT_EQUAL(-1, Oct_to_Bin(8));//Pass Cases
+  TEST_ASSERT_EQUAL(-1, Oct_to_Bin(9));//Pass Cases
 
 
 }
 
-void test_fail_bin_to_hex(void){
-
-  TEST_ASSERT_EQUAL(-1, Bin_to_Hex(123));//Pass Cases
-  TEST_ASSERT_EQUAL(-1, Bin_to_Hex(32));//Pass Cases
+void test_fail_Oct_to_Hex(void){
+  TEST_ASSERT_EQUAL_HEX(0xffffffff, Oct_to_Bin(8));//Pass Cases
+  TEST_ASSERT_EQUAL_HEX(0xffffffff, Oct_to_Bin(9));//Pass Cases
 }
-
 
 int main(void)
 {
 /* Initiate the Unity Test Framework */
   UNITY_BEGIN();
 
-/* Run Test functions */
-  //RUN_TEST(test_bin_to_dec);
-  //RUN_TEST(test_dec_to_bin); 
-  //RUN_TEST(test_Dec_to_Oct);
-  //RUN_TEST(test_Oct_to_Dec);
-  //RUN_TEST(test_Oct_to_Bin);
-    //RUN_TEST(test_bin_to_oct);
-    RUN_TEST(test_bin_to_hex);
-  //RUN_TEST(test_fail_bin_to_dec);  
- // RUN_TEST(test_fail_dec_to_bin);
- // RUN_TEST(test_fail_Dec_to_Oct);
- // RUN_TEST(test_fail_Oct_to_Dec);
- // RUN_TEST(test_fail_Oct_to_Bin);
-    RUN_TEST(test_fail_bin_to_hex);
+/* Run Test functions *//*
+  RUN_TEST(test_Bin_to_Dec);
+  RUN_TEST(test_Bin_to_Oct);
+  RUN_TEST(test_Bin_to_Hex);
+  RUN_TEST(test_Dec_to_Bin); 
+  RUN_TEST(test_Dec_to_Oct);
+  RUN_TEST(test_Dec_to_Hex);
+  RUN_TEST(test_Oct_to_Dec);
+  RUN_TEST(test_Oct_to_Bin);*/
+  
+  
+  RUN_TEST(test_fail_bin_to_dec);  
+  RUN_TEST(test_fail_bin_to_oct);
+  RUN_TEST(test_fail_bin_to_hex);
+  //RUN_TEST(test_fail_dec_to_bin);
+  //RUN_TEST(test_fail_Dec_to_Oct);
+  RUN_TEST(test_fail_Oct_to_Dec);
+  RUN_TEST(test_fail_Oct_to_Bin);
+  RUN_TEST(test_fail_Oct_to_Hex);
   /* Close the Unity Test Framework */
   return UNITY_END();
 }
--- 3_Implementation/src/logic.c
+++ 3_Implementation/src/logic.c
@@ -25,25 +25,18 @@ long int Bin_to_Dec(long int bin)
 
 }
 
-
-long int Bin_to_Oct(long int bin)
-{   long int octal = 0, decimal = 0, i = 0;
-
-    // converting binary to decimal
-    while (bin != 0) {
-        decimal += (bin % 10) * pow(2, i);
-        ++i;
-        bin /= 10;
+long int Bin_to_Oct(long int binarynum)
+{   
+    long int  octalnum = 0, j = 1, remainder;
+    while (binarynum != 0)
+    {
+        remainder = binarynum % 10;
+        if(remainder != 0 && remainder != 1){return -1;}
+        octalnum = octalnum + remainder * j;
+        j = j * 2;
+        binarynum = binarynum / 10;
     }
-    i = 1;
-
-    // converting to decimal to octal
-    while (decimal != 0) {
-        octal += (decimal % 8) * i;
-        decimal /= 8;
-        i *= 10;
-    }
-    return octal;
+    return octalnum;
 }
  
 long int Bin_to_Hex(long int bin)
@@ -61,9 +54,9 @@ long int Bin_to_Hex(long int bin)
 }
  
 long int Dec_to_Bin(long int dec)
-{
+{  
     long int bin = 0;
-    int remainder, temp = 1;
+    long int remainder, temp = 1;
 
     while (dec!=0)
     {
@@ -73,7 +66,7 @@ long int Dec_to_Bin(long int dec)
         temp = temp * 10;
     }
     return bin;
-}
+    }
  
 long int Dec_to_Oct(long int dec)
 {
@@ -96,27 +89,13 @@ long int Dec_to_Hex(long int dec)
     return hexa;   
 }
  
-long int Oct_to_Dec(long int oct)
-{
-    long int dec = 0, i = 0;
-
-    while(oct != 0)
-    {
-        dec += (oct%10) * pow(8,i);
-        ++i;
-        oct/=10;
-    }
-
-    i = 1;
-
-    return dec;
-}
-
 long int Oct_to_Bin(long int oct)
 {
     long int dec = 0, i = 0;
     long  bin = 0;
-
+    if((oct %10) == 8 || (oct % 10) == 9 )
+    {return -1;}
+    else{
     // converting octal to decimal
     while (oct != 0) {
         dec += (oct % 10) * pow(8, i);
@@ -133,52 +112,31 @@ long int Oct_to_Bin(long int oct)
     }
     return bin;
 }
+}
+
+long int Oct_to_Dec(long int oct)
+{
+    if((oct %10) == 8 || (oct % 10) == 9 ){
+        return -1;
+    }
+    else{
+        long int dec = 0, i = 0;
+        while(oct != 0){
+            dec += (oct%10) * pow(8,i);
+            ++i;
+            oct/=10;
+        }
+
+        i = 1;
+
+        return dec;
+    }
+}
 
 long int Oct_to_Hex(long int oct)
 {
-    int rem[50],len=0,decimal=0,i=0,num,ans=0;
-    while(oct!=0)
-    {
-        ans=oct % 10;
-        decimal = decimal + ans * pow(8,i);
-        i++;
-        oct = oct/10;
-    }
-    i=0;
-    while(decimal!=0)
-    {
-        rem[i]=decimal%16;
-        decimal=decimal/16;
-        i++;
-        len++;
-    }
-    printf("\nEquivalent Hexa-Decimal Number : ");
-    for(i=len-1;i>=0;i--)
-    {
-        switch(rem[i])
-        {
-            case 10:
-                printf("A"); break;
-             
-            case 11:
-                printf("B"); break;
-                 
-            case 12:
-                printf("C"); break;
-                 
-            case 13:
-                printf("D"); break;
-                 
-            case 14:
-                printf("E"); break;
-                 
-            case 15:
-                printf("F"); break;
-                 
-            default:
-                printf("%d",rem[i]);
-        }
-         
-    }
+    long int bin = Oct_to_Bin(oct);
+    long int hex = Bin_to_Hex(bin);
+    return hex; 
 }
  
